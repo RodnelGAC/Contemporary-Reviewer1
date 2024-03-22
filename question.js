d9c8ca4cc291ad9e9b0c2375c6b044349076727f
RodnelGAC <164328318+RodnelGAC@users.noreply.github.com>
2024-03-22 09:47:05 +0800

    Add files via upload

---
+++ question.js
@@ -0,0 +1,188 @@
+const quiz = [
+    {
+        q:'The process by which businesses or other organizations develop international influence or start operating in international scale?',
+        option:['JAMIE GUTTIEREZ – ANG (2018)','MANFRED STEGER (2014)', 'Albert Eeinstein', 'Jeremy Steger'],
+        answer:0
+    },
+    {
+        q:'The expansion and intensification of social relations and consciousness across world time and world-space?',
+        option:['JAMIE GUTTIEREZ – ANG (2018)','MANFRED STEGER (2014)', 'Albert Eeinstein', 'Jeremy Steger'],
+        answer: 1
+    },
+    {
+        q:'The speedup of movements and exchanges (of human beings, goods, and services, capital, technologies or cultural practices) all over the planet?',
+        option:['Globalization','INDUSTRIAL','ECONOMIC','FINANCIAL'],
+        answer:0
+    },
+    {
+        q:'Realization of a global common market, based on the freedom of exchange of goods and capital?',
+        option:['Globalization','INDUSTRIAL','ECONOMIC','FINANCIAL'],
+        answer:2
+    },
+    {
+        q:'Emergence of worldwide financial markets and better access to external financing for borrowers; emergence of under or un-regulated foreign exchange and speculative markets?',
+        option:['Globalization','INDUSTRIAL','ECONOMIC','FINANCIAL'],
+        answer:3
+    },
+    {
+        q:'which regulate the relationships among governments and guarantees the rights arising from social and economic globalization?',
+        option:['POLITICAL','INDUSTRIAL','ECONOMIC','FINANCIAL'],
+        answer:0
+    },
+    {
+        q:'Survival in the new global business market calls for improved productivity and increased competition?',
+        option:['POLITICAL','INDUSTRIAL','ECONOMIC','LEGAL/ETHICAL'],
+        answer:3
+    },
+    {
+        q:'Growth of cross-cultural contacts the desire to increase one`s standard of living and enjoy foreign products and ideas, adopt new technology and practices, and participate in a world culture?',
+        option:['POLITICAL','INDUSTRIAL','CULTURAL','LEGAL/ETHICAL'],
+        answer:2
+    },
+    {
+        q:'The start of global environmental challenges that might be solved with international cooperation, climate change, cross-boundary water air pollution over-fishing of the ocean spread of invasive species factories are built in developing countries with less environmental regulation which may increase pollution?',
+        option:['POLITICAL','ECOLOGICAL','CULTURAL','LEGAL/ETHICAL'],
+        answer:2
+    },
+    {
+        q:'Increase circulation by people of all nations with fewer restrictions. Spreading of multiculturalism, and better individual access to cultural diversity through media?',
+        option:['SOCIAL','ECOLOGICAL','CULTURAL','LEGAL/ETHICAL'],
+        answer:0
+    },
+    {
+        q:'What is ASEAN means?',
+        option:['Association of South East Asian Nations ','Asian of South East Association Nations','None Of The Above'],
+        answer:0
+    },
+    {
+        q:'is the process of increasing the financial integration mongst countries. Consequently, economic globalization ?',
+        option:['Economic globalization','ECOLOGICAL','SOCIAL','LEGAL/ETHICAL'],
+        answer:0
+    },
+    {
+        q:'is enhanced by the accession of multinational enterprises, which result into the rise of the profits of that global marketplace?',
+        option:['ECOLOGICAL','SOCIAL','Economic globalization','CULTURAL'],
+        answer:2
+    },
+    {
+        q:'The trade routes were made over the years so that goods from        one kingdom or country moved to another. The well known silk-route from east to        west is an example of historical factor?',
+        option:['ECOLOGICAL','HISTORICAL','SOCIAL','CULTURAL'],
+        answer:1
+    },
+    {
+        q:'What are the Countries part of the ASEAN?',
+        option:['Brunei, Cambodia, Indonesia, Laos, Malaysia, Myanmar, the Philippines, Singapore, Thailand, and Vietnam','Australia, China, The European Union, Iran, Japan, The Republic of Korea, Mauritius, Myanmar, The United States of America.', 'none of the above'],
+        answer:0
+    },
+    {
+        q:'The natural resources like minerals, coal,oil, gas, human resources, water, etc. make an important contribution in        globalisation.?',
+        option:['RESOURCES & MARKETS','ECONOMY','SOCIAL','CULTURAL'],
+        answer:0
+    },
+    {
+        q:'The political issues of a country make globalisation channelised as per political bosses?',
+        option:['RESOURCES & MARKETS','POLITICAL','CULTURAL','SOCIAL'],
+        answer:1
+    },
+    {
+        q:'The technological development in the areas of production, product mix and firms are helping organisations to expand        their operations?',
+        option:['RESOURCES & MARKETS','ECONOMY','SOCIAL','INDUSTRIAL ORGANISATION'],
+        answer:3
+    },
+    {
+        q:'The stage of technology in a particular field gives rise to import or export of products or services from or to the country. European        countries like England and Germany exported their chemical, electrical,        mechanical plants in 50s and 60s and exports high tech (then) goods to under        developed countries?',
+        option:['TECHNOLOGIES','POLITICAL','RESOURCES & MARKETS','SOCIAL'],
+        answer:0
+    },
+    {
+        q:'What are the three Major International Economic Organizations? Please Choose down below.',
+        option:['World Bank, Internal Monetary Fund, World Trade Organizations','INDUSTRIAL, FINANCIAL, COMPETITION, CULTURAL','NONE OF THE ABOVE'],
+        answer:0
+    },
+    {
+        q:'The development of overseas markets and manufacturing plants in autos, four wheelers and two wheelers is a classical?',
+        option:['POLITICAL','TECHNOLOGIES','PRODUCTION ISSUES','SOCIAL'],
+        answer:2
+    },
+    {
+        q:'which had previously not been active in U.N. activities, has shifted its position toward attaching importance to them under the Gorbachev Administration. Similarly, the United States under the Bush Administration, has proved its cooperative attitude with the U.N., indicating that it will pay up its arrears to the U.N. In this connection, at the 44th session of the General Assembly in 1989?',
+        option:['POLITICAL','PRODUCTION ISSUES','THE SOVIET UNION','SOCIAL'],
+        answer:2
+    },
+    {
+        q:'Before, in order to easily trade from one kingdom to another, they created the trade routes. The well known silk-route from east to west is an example of historical factor.?',
+        option:['POLITICAL','SOCIAL','FINANCIAL','HISTORICAL'],
+        answer:3
+    },
+    {
+        q:'The movement of goods determine the cost of goods and values to the end user. The overall economic trade is an important factor in globalization?',
+        option:['POLITICAL','HISTORICAL','FINANCIAL','ECONOMY'],
+        answer:3
+    },
+    {
+        q:'Usage of developed limits of creation, drowsiness in local market and over generation makes a fabricating organization search externally and go worldwide?',
+        option:['GENERATION ISSUES','ECONOMY','POLITICAL','FINANCIAL'],
+        answer:0
+    },
+    {
+        q:'The political issues of a nation make globalization channelized according to political pioneer?',
+        option:['GENERATION ISSUES','ECONOMY','POLITICAL','FINANCIAL'],
+        answer:2
+    },
+    {
+        q:'The innovative improvement in the zones of generation, item blend and firms are helping associations to extend their tasks?',
+        option:['GENERATION ISSUES','ECONOMY','MECHANCAL ASSOCIATION','FINANCIAL'],
+        answer:2
+    },
+    {
+        q:'The phase of innovation in a specific field offers ascend to import or fare of items or administrations from or to the nation. European nations like Britain and Germany sent out their synthetic, electrical, mechanical plants in 60s and fares cutting edge products to immature nations?',
+        option:['GENERATION ISSUES','ADVANCES','POLITICAL','FINANCIAL'],
+        answer:1
+    },
+    {
+        q:'Who is the american sociologst that described hign-income nations as the core of the world economy?',
+        option:['ALBER EINSTEIN','IMMANUEL WALLERSTEIN','MELIVA MARIC','ELSA EINSTEIN'],
+        answer:1
+    },
+    {
+        q:'widely thought to improve the allocation of resources, promote technology transfer, and improve living standards?',
+        option:['GENERATION ISSUES','GLOBAL ECONOMIC INTEGRATION','POLITICAL','FINANCIAL'],
+        answer:1
+    },
+    {
+        q:'has been blamed for growing trade imbalances, increased financial market volatility, and less effective domestic macroeconomic policies.  ?',
+        option:['GENERATION ISSUES','ECONOMY','GLOBAL ECONOMIC INTEGRATION','FINANCIAL'],
+        answer:2
+    },
+    {
+        q:'Attributes of Global Corporations?',
+        option:['VALUE OPPORTUNITY TO EXPAND, UNDERSTAND DIFFERENT CULTURES, TURBO-CHARGE BY THE INTERNET, CAREFULLY CHOSEN INTERNATIONAL PARTNERS, MEASURE SUCCESS, THINK GLOBALLY','World Bank, Internal Monetary Fund, World Trade Organizations','COMPETITION, CULTURAL, SOCIAL, ECOLOGICAL, TECHNICAL','NONE OF THE ABOVE'],
+        answer:0
+    },
+    {
+        q:'What SAARC means?',
+         option:['SOUTH ASSOCIATION ASIA REGIONAL COOPERATION','SOUTH ASIA ASSOCIATION REGION COOPERATION','SOUTH ASIA ASSOCIATION REGIONAL COOPERATION','SOUTH ASIA ASSOCIATION REGIONAL CORPERATION'],
+         answer:3
+     },
+    // {
+    //     q:'?',
+    //     option:['GENERATION ISSUES','ECONOMY','GLOBAL ECONOMIC INTEGRATION','FINANCIAL'],
+    //     answer:2
+    // },
+    // {
+    //     q:'?',
+    //     option:['GENERATION ISSUES','ECONOMY','GLOBAL ECONOMIC INTEGRATION','FINANCIAL'],
+    //     answer:2
+    // },
+    // {
+    //     q:'?',
+    //     option:['GENERATION ISSUES','ECONOMY','GLOBAL ECONOMIC INTEGRATION','FINANCIAL'],
+    //     answer:2
+    // },
+    
+]
+// {
+//     q:'?',
+//     option:['GENERATION ISSUES','ECONOMY','GLOBAL ECONOMIC INTEGRATION','FINANCIAL'],
+//     answer:2
+// },
